9ee23538e4db6a480613011819a6220d6751e21a
Quanglh97 <quanglehoi@gmail.com>
2021-05-11 15:26:00 +0700

    update

--- TestThread.py
+++ TestThread.py
@@ -1,51 +1,84 @@
-import logging
-import threading
-import time
-import concurrent.futures
-from bs4 import BeautifulSoup
+import requests
 from time import sleep
-import time
-import random
-from selenium import webdriver
-import selenium
-import io, json 
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-import pychrome
-from bs4 import BeautifulSoup
 
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+url = "http://192.168.100.233/ISAPI/PTZCtrl/channels/1/continuous"
 
-def thread_function(name, url):
-    logging.info("Thread %s: starting", name)
-    time.sleep(2)
-    logging.info("Thread %s: finishing", name)
-    driver = webdriver.Chrome(executable_path =r"C:\Users\HLC_2021\Desktop\chromedriver.exe")
-    driver.get(url)
-    #sleep(10)
+payloadLeftAbove = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><PTZData><pan>-60</pan><tilt>60</tilt></PTZData>"
+payloadAbove = '<?xml version: \"1.0\" encoding="UTF-8"?><PTZData><pan>0</pan><tilt>60</tilt></PTZData>'
+payloadRightAbove = "<?xml version:\"1.0\" encoding=\"UTF-8\"?><PTZData><pan>60</pan><tilt>60</tilt></PTZData>"
 
-url = "https://stackoverflow.com"
+payloadLeft = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><PTZData><pan>-60</pan><tilt>0</tilt></PTZData>"
+payloadRight = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><PTZData><pan>60</pan><tilt>0</tilt></PTZData>"
 
-list = ["https://stackoverflow.com/questions/41354472/multi-threading-in-selenium-python",  "https://stackoverflow.com", "https://fate0.github.io/pychrome/#getting-started"]
+payloadLeftBottom = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><PTZData><pan>-60</pan><tilt>-60</tilt></PTZData>"
+payloadBottom = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><PTZData><pan>0</pan><tilt>-60</tilt></PTZData>"
+payloadRightBottom = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><PTZData><pan>60</pan><tilt>-60</tilt></PTZData>"
+
+payloadCancel = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><PTZData><pan>0</pan><tilt>0</tilt></PTZData>"
+
+payloadAutoPan = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><autoPanData><autoPan>60</autoPan></autoPanData>"
+payloadStopAutoPan = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><autoPanData><autoPan>0</autoPan></autoPanData>"
+
+payloadZoomOut = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><PTZData><zoom>-60</zoom></PTZData>"
+payloadZoomIn = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><PTZData><zoom>60</zoom></PTZData>"
+payloadZoomStop = "<?xml version: \"1.0\" encoding=\"UTF-8\"?><PTZData><zoom>0</zoom></PTZData>"
+
+headers = {
+  'Connection': 'keep-alive',
+  'Cache-Control': 'max-age=0',
+  'Accept': '*/*',
+  'X-Requested-With': 'XMLHttpRequest',
+  'If-Modified-Since': '0',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  'Origin': 'http://192.168.100.233',
+  'Referer': 'http://192.168.100.233/doc/page/preview.asp',
+  'Accept-Language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+  'Cookie': 'language=vi; WebSession_872ecca789=ab703ef7be5a702c342da7d123147fe4c903ea96bb8292bd6e39d422bafdd55a; _wnd_size_mode=4'
+}
+
+def leftAboveMove():
+    response = requests.request("PUT", url, headers=headers, data=payloadLeftAbove)
+    sleep(0.5)
+    response = requests.request("PUT", url, headers=headers, data=payloadCancel)
+
+def AboveMove():
+    response = requests.request("PUT", url, headers=headers, data=payloadAbove)
+    sleep(0.5)
+    response = requests.request("PUT", url, headers=headers, data=payloadCancel)
+
+def rightAboveMove():
+    response = requests.request("PUT", url, headers=headers, data=payloadRightAbove)
+    sleep(0.5)
+    response = requests.request("PUT", url, headers=headers, data=payloadCancel)
+
+def rightMove():
+    response = requests.request("PUT", url, headers=headers, data=payloadRight)
+    sleep(0.5)
+    response = requests.request("PUT", url, headers=headers, data=payloadCancel)
+
+def leftMove():
+    response = requests.request("PUT", url, headers=headers, data=payloadLeft)
+    sleep(0.5)
+    response = requests.request("PUT", url, headers=headers, data=payloadCancel)
+
+def autoPanMove():
+    response = requests.request("PUT", url, headers=headers, data=payloadAutoPan)
+
+def stopAutoPanMove():
+    response = requests.request("PUT", url, headers=headers, data=payloadStopAutoPan)
+
+def zoomOut():
+    response = requests.request("PUT", url, headers=headers, data=payloadZoomOut)
+    sleep(0.5)
+    response = requests.request("PUT", url, headers=headers, data=payloadZoomStop)
+
+def zoomOut():
+    response = requests.request("PUT", url, headers=headers, data=payloadZoomIn)
+    sleep(0.5)
+    response = requests.request("PUT", url, headers=headers, data=payloadZoomStop)
+
+
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
-
-    logging.info("Main    : before creating thread")
-    x = threading.Thread(target=thread_function, args=(1,list[0]), daemon=True)
-    x2 = threading.Thread(target=thread_function, args=(2,list[0]), daemon=True)
-    logging.info("Main    : before running thread")
-    driver = webdriver.Chrome(executable_path =r"C:\Users\HLC_2021\Desktop\chromedriver.exe")
-    driver.get(url)
-    x.start()
-    x2.start()
-    sleep(10)
-    logging.info("Main    : wait for the thread to finish")
-    x.join()
-    x2.join()
-    logging.info("Main    : all done")
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-    #     executor.map(thread_function, range(3))
+    #rightAboveMove()
+    autoPanMove()
